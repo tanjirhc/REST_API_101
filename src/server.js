123d54eb0ab4db731d1c5dfb2a867cb6920154b4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-14 23:58:09 +0600

    rest client extension for api testing

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const shortid = require('shortid')
 
 const app = express()
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
+
+app.post('/', (req, res) => {
+  const player = {
+    ...req.body,
+    id: shortid.generate()
+  }
+  res.status(201).json(player)
+})
 
 app.get('/health', (_req, res) => {
   res.status(200).json({ status: 'OK'})
